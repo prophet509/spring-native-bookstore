e5c82fe017b45e5af6e8ba2a324669f7f841ccf6
Loc Pham <halfbloodprincess9@gmail.com>
2026-05-26 20:32:17 +0700

    fix: test in search-service and order-service

--- edge-service/src/test/java/com/locpham/bookstore/edgeservice/EdgeServiceApplicationTests.java
+++ edge-service/src/test/java/com/locpham/bookstore/edgeservice/EdgeServiceApplicationTests.java
@@ -4,12 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {"spring.cloud.config.enabled=false", "spring.session.store-type=none"})
 @Import(TestChannelBinderConfiguration.class)
 class EdgeServiceApplicationTests {
+
+    @MockitoBean
+    private ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
 
     @Test
     void verifyThatSpringContextLoads() {}
